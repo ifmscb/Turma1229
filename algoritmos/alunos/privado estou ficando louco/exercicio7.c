f4901aeb25df5e95d9d9b42a852832c118dbbc4a
Luis Felipe de Arruda Reis <luis.rubronegro.lf@gmail.com>
2022-08-07 21:30:24 -0300

    alguma coisa

---
+++ algoritmos/alunos/privado estou ficando louco/exercicio7.c
@@ -0,0 +1,24 @@
+// importação de bbliotecas
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+
+{
+    float const salario= 1212.00, desconto = 0.10, multa = 0.15; 
+    float watts= 0.6, teste;
+    printf("digite o watts");
+    scanf("%f",& teste);
+    
+
+
+
+
+
+
+
+
+}
