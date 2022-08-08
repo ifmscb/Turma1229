1ee43be5be3f0c4e71ec4b40e0f54d1d1f8209dc
Vinicius Souza <contato@viniccius.com.br>
2022-08-08 17:15:52 -0400

    lição 07 - lista 02

---
+++ algoritmos/alunos/osiris.souza@estuante.ifms.edu.br/lista-02/lesson-07.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+
+// função principal
+int main()
+{
+  float const salarioMinimo = 1212.00, taxaSalario = 0.05;
+  float qw, qwUsado;
+
+  printf("Quantidade de Whats usado");
+  scanf("%f"), qwUsado;
+
+  printf("o valor do w foi: ", qwUsado);
+  qw = (taxaSalario * salarioMinimo) / 100;
+}
