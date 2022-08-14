1fb58df4243ced954020d887e817c9998f0ef1b0
Vinicius Souza <contato@viniccius.com.br>
2022-08-14 17:16:47 -0400

    exerciocio 06

--- algoritmos/alunos/vinicius-sz/lista-03/lesson-06.c
+++ algoritmos/alunos/vinicius-sz/lista-03/lesson-06.c
@@ -6,5 +6,24 @@
 
 int main()
 {
-  printf("Hello World");
+
+  float moeda25 = 0.25, moeda50 = 0.5, moeda01 = 1.0;
+  float final;
+  float recebe25, recebe50, recebe01;
+
+  printf("moeda 0.25: ");
+  scanf("%f", &recebe25);
+
+  printf("moeda 0.50: ");
+  scanf("%f", &recebe50);
+  
+  printf("moeda 1.00: ");
+  scanf("%f", &recebe01);
+
+  float total = moeda25 * ;
+  float total2 = moeda50 * final; 
+  float total3 = moeda01 * final;
+
+
+  printf("valor final foi: %.2f", total);
 }
