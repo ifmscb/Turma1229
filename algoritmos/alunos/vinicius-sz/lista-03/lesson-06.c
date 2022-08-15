697d11b4a5056b8e9eebb11645ff7aceeeb6ddae
Vinicius Souza <contato@viniccius.com.br>
2022-08-14 21:41:50 -0400

    terminado exercicio 06

--- algoritmos/alunos/vinicius-sz/lista-03/lesson-06.c
+++ algoritmos/alunos/vinicius-sz/lista-03/lesson-06.c
@@ -8,22 +8,34 @@ int main()
 {
 
   float moeda25 = 0.25, moeda50 = 0.5, moeda01 = 1.0;
-  float final;
   float recebe25, recebe50, recebe01;
+  float calcOne, calcTwo, calcTree;
+  float final;
 
   printf("moeda 0.25: ");
   scanf("%f", &recebe25);
 
   printf("moeda 0.50: ");
   scanf("%f", &recebe50);
-  
+
   printf("moeda 1.00: ");
   scanf("%f", &recebe01);
 
-  float total = moeda25 * ;
-  float total2 = moeda50 * final; 
-  float total3 = moeda01 * final;
+  calcOne = moeda25 * recebe25;
+  calcTwo = moeda50 * recebe50;
+  calcTree = moeda01 * recebe01;
+  final = calcOne + calcTwo + calcTree;
+  float anual = final * 365;
 
-
-  printf("valor final foi: %.2f", total);
+  printf("A quantidade de moeda de 0.25 foi de: %.0f\n", recebe25);
+  printf("A quantidade de moeda de 0.50 foi de: %.0f\n", recebe50);
+  printf("A quantidade de moeda de 1.00 foi de: %.0f\n", recebe01);
+  
+  printf("A quantidade total de moeda de 0.25 foi de: %.2f\n", calcOne);
+  printf("A quantidade total de moeda de 0.50 foi de: %.2f\n", calcTwo);
+  printf("A quantidade total de moeda de 1.00 foi de: %.2f\n", calcTree);
+  
+  printf("Soma total: %.2f\n", final);
+  printf("Soma total anual: %.2f\n", anual );
+ 
 }
