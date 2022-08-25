7b1c52f7424570e45dbb532eb838f94eba182386
Vinicius Souza <contato@viniccius.com.br>
2022-08-25 02:29:17 -0400

    realização da atividade 1 2 3 da lista 5

---
+++ algoritmos/alunos/vinicius-sz/lista-05/lesson-02.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+
+  system("cls");
+  char name[30];
+  int idade;
+
+  printf("BEM VINDO(A) AO CAMPEONATO DE FUTEBOL DA CIDADE DE CAFUNDÓ\n");
+  printf("Antes de comecarmos, vamos descobir em qual categoria tu se encaixa.\n");
+  system("pause");
+  system("cls");
+
+  printf("Digite seu nome: ", name);
+  gets(name);
+
+  printf("Digite a tua idade(ex: 16): ");
+  scanf("%i", &idade);
+  system("cls");
+
+  if (idade == 5 || idade <= 7)
+  {
+
+    printf("Hi %s\nCom base na sua idade tu pertence a categoria:\n==>Infantil A ", name);
+  }
+  else if (idade == 8 || idade <= 10)
+  {
+
+    printf("Hi %s\nCom base na sua idade tu pertence a categoria:\n==>Infantil B ", name);
+  }
+  else if (idade == 11 || idade <= 13)
+  {
+
+    printf("Hi %s\nCom base na sua idade tu pertence a categoria:\n==>Juvenil A ", name);
+  }
+  else if (idade == 14 || idade <= 17)
+  {
+
+    printf("Hi %s\nCom base na sua idade tu pertence a categoria:\n==>Juvenil B ", name);
+  }
+  else if (idade > 18)
+  {
+
+    printf("Hi %s\nCom base na sua idade tu pertence a categoria:\n==>Adulta", name);
+  }
+  else
+  {
+    printf("Operacion Invadidad");
+  }
+}
---
+++ algoritmos/alunos/vinicius-sz/lista-05/lesson-03.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+  system("cls");
+  char name[30];
+  int idade;
+
+  printf("SISTEMA ELEITORAL DO INSITUTO NACIONAL DOS PREGUISOS (INP)\n");
+
+  printf("Digite seu nome: ");
+  scanf("%s", name);
+
+  printf("Digite sua idade(ex: 16): ");
+  scanf("%i", idade);
+  system("cls");
+
+  if (idade <= 15 || idade < 16)
+  {
+    printf("SISTEMA ELEITORAL DO INSITUTO NACIONAL DOS PREGUISOS (INP)\n");
+    printf("==> Ainda nao consta como cidadao");
+  }
+  else if (idade == 16 && idade >= 65)
+  {
+    printf("SISTEMA ELEITORAL DO INSITUTO NACIONAL DOS PREGUISOS (INP)\n");
+    printf("==> eleitor obrigatorio");
+  }
+  else if (idade >= 16 && idade <= 18 || idade > 65)
+  {
+    printf("SISTEMA ELEITORAL DO INSITUTO NACIONAL DOS PREGUISOS (INP)\n");
+    printf("==> facultativo");
+  }
+  else
+  {
+    printf("pt-br");
+  }
+}
