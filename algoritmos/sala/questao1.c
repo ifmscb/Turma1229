91468a8d06f9d769008f1417ee9079269f2dad50
OSIRIS VINICIUS MENDES DE SOUZA <05796694162@laboratorio.ifms.br>
2022-08-29 12:48:12 -0400

    revisao

---
+++ algoritmos/sala/questao1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+	int p,r;
+	int entrada;
+  printf("Hello World");
+  
+  
+  switch(entrada){
+	case 1:
+	{
+	if(r == 1 && r == 0 ){
+  	
+ } else if(p == 1 && p == 0){
+		
+	}
+		break;
+	}
+	
+	}
+  
+}
