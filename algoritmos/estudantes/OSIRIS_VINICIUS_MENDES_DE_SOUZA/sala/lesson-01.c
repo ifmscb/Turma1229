019b0489a962020efd20c070d84b2b370eb0b5d3
OSIRIS VINICIUS MENDES DE SOUZA <05796694162@laboratorio.ifms.br>
2022-09-05 08:56:27 -0400

    atv em sala

---
+++ algoritmos/estudantes/OSIRIS_VINICIUS_MENDES_DE_SOUZA/sala/lesson-01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+
+int main(){
+	int i;
+	
+	for(i = 0; i <= 10; i+=2){
+			printf("%i\n", i);
+	}
+}
